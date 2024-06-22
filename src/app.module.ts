989769fe628a154671b1b2a77dde69f01e924907
pulak-sarmah <pulaksarmah333@gmail.com>
2024-06-22 12:34:52 +0530

    chore: Add user and auth modules, resolvers, and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,34 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    UserModule,
+    GraphQLModule.forRootAsync({
+      imports: [ConfigModule, AppModule],
+      inject: [ConfigService],
+      driver: ApolloDriver,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      useFactory: async (configService: ConfigService) => {
+        return {
+          playground: true,
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: true,
+        };
+      },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://example.com',
+      'http://another-frontend-url.com',
+    ],
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS'],
+  });
+  app.use(cookieParser());
+  app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 1 }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((acc, error) => {
+          acc[error.property] = Object.values(error.constraints).join(', ');
+          return acc;
+        }, {});
+        throw new BadRequestException(formattedErrors);
+      },
+    }),
+  );
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
