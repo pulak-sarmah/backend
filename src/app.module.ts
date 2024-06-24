c58670cf7dbba604914ab47d36d558ab8f8f8f73
pulak-sarmah <pulaksarmah333@gmail.com>
2024-06-24 11:27:27 +0530

    chore: Add RedisPubSub for GraphQL subscriptions and TokenService

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,19 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { join } from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { TokenService } from './token/token.service';
+
+const pubSub = new RedisPubSub({
+  connection: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    retryStrategy: (times) => {
+      // reconnect after
+      return Math.min(times * 50, 2000);
+    },
+  },
+});
 
 @Module({
   imports: [
@@ -16,12 +29,37 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule, AppModule],
       inject: [ConfigService],
       driver: ApolloDriver,
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      useFactory: async (configService: ConfigService) => {
+
+      useFactory: async (
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        configService: ConfigService,
+        tokenService: TokenService,
+      ) => {
         return {
           playground: true,
           autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
           sortSchema: true,
+          subscriptions: {
+            'graphql-ws': true,
+            'subscriptions-transport-ws': true,
+          },
+          onConnect: (connectionParams) => {
+            const token = tokenService.extractToken(connectionParams);
+            if (!token) {
+              throw new Error('No token provided');
+            }
+            const user = tokenService.validateToken(token);
+            if (!user) {
+              throw new Error('Invalid token');
+            }
+            return { user };
+          },
+          context: ({ req, res, connection }) => {
+            if (connection) {
+              return { req, res, user: connection.context.user, pubSub }; // Injecting pubSub into context
+            }
+            return { req, res };
+          },
         };
       },
     }),
@@ -30,6 +68,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
   ],
   controllers: [],
-  providers: [],
+  providers: [TokenService],
 })
 export class AppModule {}
